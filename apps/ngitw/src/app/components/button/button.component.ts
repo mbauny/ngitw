7f7cd181ad7c91d9741cb687b0a212ae615b8114
mbauny <mbauny@users.noreply.github.com>
2022-05-09 22:57:27 +0200

    global counter

--- apps/ngitw/src/app/components/button/button.component.ts
+++ apps/ngitw/src/app/components/button/button.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ClickCountService } from '../../services/click-count/click-count.service';
 
 @Component({
   selector: 'ngitw-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit, OnDestroy {
   count = 0;
+  sub = new Subscription();
 
-  constructor() {
+  constructor(private readonly click: ClickCountService) {
     //
   }
 
   onClick(): void {
-    this.count += 1;
+    this.click.up();
+  }
+
+  ngOnInit(): void {
+    this.sub.add(this.click.counter.subscribe((value) => (this.count = value)));
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
